6f04487774ec2b525d009b0bfc6e7a12550ab0da
Chloe <connorm400@stemia.ca>
2025-07-21 17:47:07 -0600

    added shebang support

--- main.c
+++ main.c
@@ -1,13 +1,37 @@
 #include "term.h"
 #include "parse.tab.h"
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+extern FILE *yyin; // used by lex
 
 int main(int argc, char **argv) 
 {
+    yyin = stdin;
+
     for (int i = 1; i < argc; i++) {
         if (!strcmp(argv[i], "-hide-steps")) 
             hide_steps = true;
+
+        if (!strcmp(argv[i], "-run")) {
+            // ran with a shebang
+            if (yyin != stdin) {
+                fprintf(stderr, "error: you may only run one file\n");
+                exit(1);
+            }
+            const char *file_path = argv[i+1];
+            FILE *f = fopen(file_path, "r");
+            if (!f) {
+                fprintf(stderr, "error opening file %s\n", file_path);
+                exit(1);
+            }
+            yyin = f;
+        }
+
     }
     yyparse();
+
+    if (yyin != stdin) fclose(yyin);
     return 0;
 }
--- readme.md
+++ readme.md
@@ -6,12 +6,19 @@ $ git clone https://github.com/omnomctr/lc.git
 $ cd lc
 $ make
 ... (will download std_ds.h)
-$ rlwrap ./lc # (if you want readline)
+$ sudo cp lc /usr/bin/
+$ lc
+(\x.x a)
+0: (λx.x a)
+1: a
+done
+$
 ```
 
 Run the example program `fact.lc`:
 ```sh
-$ ./lc <fact.lc
+$ chmod +x fact.lc
+$ ./fact.lc
 .... (will output each step, this might take a while)
 11382: λf.λx.(f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f (f x))))))))))))))))))))))))
 done
